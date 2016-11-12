1cc94ed1fd371e884928d2c11c202e85ed2f3c21
keeev <hi@augusteight.at>
2016-11-12 18:01:15 +0100

    resolve conflicts; add dependencies and update gitignore

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const electron = require('electron')
+
+
+// Module for Navigation and Stuff.
+const Menu = electron.Menu
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -13,7 +18,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    titleBarStyle: 'hidden-inset'
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
